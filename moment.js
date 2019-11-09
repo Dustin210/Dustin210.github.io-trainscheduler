7b1101afd24b0e0ea28b4784c2fd76865f516619
Dustin Martinez <martinezdustin736@gmail.com>
2019-11-08 22:48:09 -0600

    updated files

--- moment.js
+++ moment.js
@@ -1,4 +1,4 @@
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAJYZNvZ6K91UsDwoPAP2ONhecLjcIFag0",
     authDomain: "project-test-8ef8d.firebaseapp.com",
     databaseURL: "https://project-test-8ef8d.firebaseio.com",
@@ -7,6 +7,7 @@ var firebaseConfig = {
     messagingSenderId: "458864230043",
     appId: "1:458864230043:web:9005130a6c98d132cbd03e",
     measurementId: "G-17J3LV62RD"
+  
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
@@ -15,29 +16,46 @@ var firebaseConfig = {
  
 
   $("#addTrainBtn").on("click", function() {
-      var trainName = $("#train-Name").val().trim();
+      var trainName = $("#trainName").val().trim();
       var destination = $("#destination").val().trim();
-      var firstTrain = moment($("#first-Train").val().trim(),"HH:mm").subtract(10,"years").format("X");
-      var frequency = $("#freq").val().trim();
+      var firstTrain = moment($("#firstTrain").val().trim(),"HH:mm").subtract(10,"years").format("X");
+      var frequency = $("#frequency").val().trim();
 
       var newTrain = {
           name: trainName,
           destination: destination,
           firstTrain: firstTrain,
-          freq: freq
+          frequency: frequency
       }
 
       trainData.ref().push(newTrain);
 
       alert("Train added!");
 
-      $("#train-Name").val("");
+      $("#trainName").val("");
       $("#destination").val("");
-      $("#first-Train").val("");
-      $("#freq").val("");
+      $("#firstTrain").val("");
+      $("#frequency").val("");
 
 
       return false;
+      
+  })
+
+  trainData.ref().on("child_added",function(snapshot){
+      var name = snapshot.val().name;
+      var destination = snapshot.val().destination;
+      var frequency = snapshot.val().frequency;
+      var firstTrain = snapshot.val().firstTrain;
+
+      var remainder = moment().diff(moment.unix(firstTrain),"minutes")%frequency;
+      var minutes = frequency - remainder;
+      var arrival = moment().add(minutes,"m").format("hh:mm A");
+      console.log(remainder);
+      console.log(minutes);
+      console.log(arrival);
+      
+      
       
   })
   
